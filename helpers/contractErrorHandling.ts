e833d619abc144e85cdb566743ca73888e130613
Antonis Thomas <iorulezz@gmail.com>
2023-01-08 13:12:45 +0100

    fixed error message

--- helpers/contractErrorHandling.ts
+++ helpers/contractErrorHandling.ts
@@ -1,11 +1,12 @@
 export const parseError = (errMsg: string) => {
-  const errorRegex = /error=(\{.+\})/;
+  const errorRegex = /reason="([^"]+)/;
   const errorMatch = errMsg.match(errorRegex);
   if (errorMatch) {
     const errorString = errorMatch[1];
-    const error = JSON.parse(errorString);
+    return errorString;
+    //const error = JSON.parse(errorString);
     
-    if (error.data.message) return error.data.message;
+    //if (error.data.message) return error.data.message;
   } else if (errMsg.includes("ACTION_REJECTED")) {
     return "User rejected the transaction."
   }
